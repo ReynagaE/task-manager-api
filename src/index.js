01f12f28bd97c91c8e08a96e9c6bb6a61ac38a76
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-18 12:49:10 -0600

    Trying HTTPS PORT

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
-import https from 'https'
 
-const server = https.createServer(app)
 async function main() {
   try {
     await connectDB();
-    server.listen(PORT);
-    console.log(`Listening on port https://localhost:${PORT}`);
+    app.listen(PORT);
+    console.log(`Listening on port http://localhost:${PORT}`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
     console.error(error);
   }
 }
-
 
 
 main();
