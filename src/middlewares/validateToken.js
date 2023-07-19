ccd1cb71a9d6cf3c273a01070edc5d8194962a2b
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 11:08:33 -0600

    No httpOnly

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,11 +4,11 @@ import { TOKEN_SECRET } from "../config.js";
 export const authRequired = (req, res, next) => {
   const authHeader = req.header("Authorization");
 
-  if (!authHeader) return res.status(401).json({ message: "No token, authorization denied" });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-  const token = authHeader.split(" ")[1]; // Extraemos el token del encabezado
-
-  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+  const token = authHeader.split(" ")[1];
 
   jwt.verify(token, TOKEN_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Invalid token" });
