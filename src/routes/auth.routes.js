9534337ea00a1cfd8d4fa536b82a62e55a6b71f9
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-18 19:21:19 -0600

    no domain

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { register, login, verifyToken, profile,  } from "../controllers/auth.controller.js";
+import { register, login, verifyToken} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/verify", verifyToken);
-router.get("/profile", profile);
 
 
 export default router;
