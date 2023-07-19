358f79f9845355b15627f8c41567dc7cd6e4f601
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 11:02:24 -0600

    No httpOnly

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,7 +2,11 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) return res.status(401).json({ message: "No token, authorization denied" });
+
+  const token = authHeader.split(" ")[1]; // Extraemos el token del encabezado
 
   if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
