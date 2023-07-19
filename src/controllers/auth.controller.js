53d5588ada60177d1e83ba77a50baa6496ac3e3f
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 10:17:28 -0600

    No httpOnly

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,7 +26,7 @@ export const register = async (req, res) => {
     
     res.cookie("token", token, {
       //httpOnly: process.env.NODE_ENV !== "development",
-      secure: true,
+      //secure: true,
       sameSite: "none"
     });
 
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       //httpOnly: process.env.NODE_ENV !== "development",
-      secure: true,
+      //secure: true,
       sameSite: "none",
     });
 
@@ -79,7 +79,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.cookie("token", "", {
     //httpOnly: true,
-    secure: true,
+    //secure: true,
     expires: new Date(0),
   });
   return res.sendStatus(200);
